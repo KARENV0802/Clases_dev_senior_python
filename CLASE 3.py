50709069d3d7f28a02cb27802dc7c018e0f4ee2a
KARENV0802 <ramirezavilakarenvanessa@gmail.com>
2024-11-15 16:08:08 -0500

    CODIGO CLASE 3

---
+++ CLASE 3.py
@@ -0,0 +1,9 @@
+age = 18
+country = "COL"
+
+if age >= 21 and country == "usa" :
+    print("you can buy alcohol")
+elif age >= 18 and country == "COL":
+    print("you can buy alcohol")
+else: 
+    print("you can not buy alcohol")
